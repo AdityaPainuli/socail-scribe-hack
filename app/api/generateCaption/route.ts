2c9f5fb520d3fccaa4295229967787eba36d7529
Aditya Painuli <adityapainuli2004@gmail.com>
2024-02-05 06:00:01 +0530

    changing max-token

--- app/api/generateCaption/route.ts
+++ app/api/generateCaption/route.ts
@@ -29,7 +29,7 @@ export async function POST(req: Request, res: Response) {
     ],
     model: "gpt-4",
     temperature: 1,
-    max_tokens: 1024,
+    max_tokens: 256,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
