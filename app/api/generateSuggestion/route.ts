60d42d51cc738bb0ab91da35a348f1816c97f475
Aditya Painuli <adityapainuli2004@gmail.com>
2024-02-05 06:05:55 +0530

    changed token for suggestions

--- app/api/generateSuggestion/route.ts
+++ app/api/generateSuggestion/route.ts
@@ -51,7 +51,7 @@ export async function POST(req: Request, res: Response) {
       {
         role: "system",
         content: `
-          Generate new LinkedIn post suggestions based on your past data. Provide the topic and details of your previous posts, including target audience, tone, and keywords. Our AI will analyze your data to generate 5-6 tailored suggestions to help you create engaging content for your audience
+          Generate new LinkedIn post suggestions based on your past data. Provide the topic and details of your previous posts, including target audience, tone, and keywords. Our AI will analyze your data to generate 3-4 tailored suggestions to help you create engaging content for your audience
           past data = ${data}
 
 
@@ -68,9 +68,9 @@ content:"string"
       `,
       },
     ],
-    model: "gpt-3.5-turbo",
+    model: "gpt-4",
     temperature: 1,
-    max_tokens: 1024,
+    max_tokens: 600,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
